42612fc90893098c8aa154f5894b95cfa472e89b
Vitor Ângelo <vitorangelodepaula@gmail.com>
2026-04-05 20:58:07 +0000

    adiciona `jest` e configura test scripts

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Vitoria, eu te amo</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
